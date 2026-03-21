41b1d3e6ef62c254d99dc80fa3bfc498bdec679e
adnan <firsttleet1337@gmail.com>
2026-03-21 09:23:37 +0000

    responseBuilder header

---
+++ server/request-response/response/ResponseBuilder.hpp
@@ -0,0 +1,45 @@
+#ifndef RESPONSE_BUILDER_HPP
+#define RESPONSE_BUILDER_HPP
+
+
+#include "Request.hpp"
+#include "RequestParser.hpp"
+#include "Response.hpp"
+#include "../server/core/Config.hpp"
+#include <string>
+#include <sys/stat.h>
+
+class ResponseBuilder {
+    public:
+        ResponseBuilder();
+
+        Response dispatch(const Request& req, const ServerConfig& config);
+        Response buildError(int code, const ServerConfig& config);
+
+    private:
+        const LocationConfig* matchRoute(const std::string&  path,
+                                    const ServerConfig& config) const;
+        std::string statusMessage(int code);
+
+        bool fileExists(const std::string& path) const;
+        std::string readFile(const std::string& path) const;
+        std::string sizeToString(std::size_t n);
+        std::string resolve_path(std::string root, std::string path);
+        Response buildeResfromOutput(std::string raw, const ServerConfig& config);
+
+        Response handleGet(const Request&      req,
+                       const LocationConfig& route,
+                       const ServerConfig&  config);
+
+        Response handlePost(const Request&      req,
+                        const LocationConfig& route,
+                        const ServerConfig&  config);
+
+        Response handleDelete(const Request&      req,
+                          const LocationConfig& route,
+                          const ServerConfig&  config);
+        
+
+};
+
+#endif
