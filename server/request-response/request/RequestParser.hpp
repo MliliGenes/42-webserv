69dda2bdb33ed2d002440312a3e94f07ff4a32be
adnan <firsttleet1337@gmail.com>
2026-03-21 09:20:50 +0000

    requestParser header

---
+++ server/request-response/request/RequestParser.hpp
@@ -0,0 +1,48 @@
+#ifndef REQUEST_PARSER_HPP
+#define REQUEST_PARSER_HPP
+
+#include "Request.hpp"
+#include <string>
+
+static const char* allowedMethode[] = {"POST", "GET", "DELETE", NULL};
+static const char* allowedVersion[] = {"HTTP/1.1", "HTTP/1.0", NULL};
+
+class RequestParser {
+
+    public:
+    enum Status { Incomplete, Complete, Error };
+
+    RequestParser(std::size_t max_body_size = 1024 * 1024);
+    ~RequestParser();
+
+    Status feed(const char* data, std::size_t len);
+
+    const Request& getRequest()     const;
+    int getErrorCode() const;
+    void reset();
+    void setMaxBodySize(std::size_t size);
+    
+    static std::string ft_trim(const std::string& s);
+    static std::string ft_toLower(const std::string& s);
+    private:
+        std::string  buffer_;
+        Request      request_;
+        bool         request_line_parsed_;
+        bool         headers_parsed_;
+        bool         chunked_;
+        std::size_t  content_length_;
+        std::string  chunk_buffer_;
+        std::size_t  max_body_size_;
+        int          error_code_;
+
+        int parseRequestLine();
+        int parseHeaders();
+        Status handleBody();
+        bool decodeChunked();
+
+        bool isAllowedMethod(const std::string& methode) const;
+        bool isAllowedVersion(const std::string& version) const;
+
+};
+
+#endif
