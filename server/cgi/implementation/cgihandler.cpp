1937fac181d72429bdc5a069005cf2e8db642c00
abdennour <abdnourchatte99@gmail.com>
2026-04-02 16:40:42 +0100

    issues with the config

--- server/cgi/implementation/cgihandler.cpp
+++ server/cgi/implementation/cgihandler.cpp
@@ -1,2 +1,5 @@
 // here ill implemte the main cgi handler, which will be used to handle all cgi requests
-#include "cgihandler.hpp"
+#include "../include/cgihandler.hpp"
+
+
+//response cgihandeler(const requist& req, const std::string& fs_path, const location& loc) // her is the main function 
