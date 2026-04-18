2f6d2187cfb419b1ea016823c303a156255e540f
adnan <firsttleet1337@gmail.com>
2026-04-18 10:25:35 +0100

    main

--- main.cpp
+++ main.cpp
@@ -2,28 +2,47 @@
 #include "include/TrpSchema.hpp"
 #include "config/configSchema.cpp"
 
+#include "server/core/Config.hpp"
+#include "server/core/Server.hpp"
+
+void ll() {system("leaks webserv");}
+
 int main (int ac, char ** av) {
+    atexit(ll);
     TrpJsonParser parser;
-    if (ac == 1)
-        parser.setLexer(new TrpJsonLexer("./config/minimal.config.json"));
-    else
-        parser.setLexer(new TrpJsonLexer(av[1]));
 
-    if (!parser.parse())
+    TrpJsonLexer* lexer = NULL;
+    if (ac == 1){
+        lexer = new TrpJsonLexer("config/minimal.config.json");
+    }
+    else {
+        lexer = new TrpJsonLexer(av[1]);
+    }
+
+    parser.setLexer(lexer);
+
+    if (!parser.parse()) {
+        std::cerr << "Failed to parse JSON file." << std::endl;
         return 1;
+    }
 
-    parser.prettyPrint();
-    
     TrpValidatorContext ctx;
     if (!serversConfigArray.validate(parser.getAST(), ctx)) {
+        std::cerr << "\n--- Validation Errors ---" << std::endl;
+        parser.prettyPrint();
         ctx.printErrors();
         return 1;
     }
 
     try {
-    // * start(); this should do everything
-    } catch (std::exception &e) {
+        Config configs(parser.getAST());
+        // configs.prettyPrint();
         
+        Server server(configs);
+        server.run();
+    } catch (std::exception &e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
     }
 
     return 0;
